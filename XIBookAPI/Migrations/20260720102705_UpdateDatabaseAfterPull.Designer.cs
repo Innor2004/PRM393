5d8a5cabb223bde9dba6e665b22f48a24d12ea03
sontthe173446-sudo <sontthe173446@fpt.edu.vn>
2026-07-20 18:06:25 +0700

    Save current work before merging main

--- XIBookAPI/Migrations/20260720102705_UpdateDatabaseAfterPull.Designer.cs
+++ XIBookAPI/Migrations/20260720102705_UpdateDatabaseAfterPull.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhysicsBook.Data;
 
@@ -11,9 +12,11 @@ using PhysicsBook.Data;
 namespace XIBookAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260720102705_UpdateDatabaseAfterPull")]
+    partial class UpdateDatabaseAfterPull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ XIBookAPI/Migrations/20260720102705_UpdateDatabaseAfterPull.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XIBookAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabaseAfterPull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Progress_Lessons_LessonId",
+                table: "Progress");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Progress_Users_UserId",
+                table: "Progress");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Progress",
+                table: "Progress");
+
+            migrationBuilder.RenameTable(
+                name: "Progress",
+                newName: "Progresses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Progress_UserId",
+                table: "Progresses",
+                newName: "IX_Progresses_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Progress_LessonId",
+                table: "Progresses",
+                newName: "IX_Progresses_LessonId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Progresses",
+                table: "Progresses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Progresses_Lessons_LessonId",
+                table: "Progresses",
+                column: "LessonId",
+                principalTable: "Lessons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Progresses_Users_UserId",
+                table: "Progresses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Progresses_Lessons_LessonId",
+                table: "Progresses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Progresses_Users_UserId",
+                table: "Progresses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Progresses",
+                table: "Progresses");
+
+            migrationBuilder.RenameTable(
+                name: "Progresses",
+                newName: "Progress");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Progresses_UserId",
+                table: "Progress",
+                newName: "IX_Progress_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Progresses_LessonId",
+                table: "Progress",
+                newName: "IX_Progress_LessonId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Progress",
+                table: "Progress",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Progress_Lessons_LessonId",
+                table: "Progress",
+                column: "LessonId",
+                principalTable: "Lessons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Progress_Users_UserId",
+                table: "Progress",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
